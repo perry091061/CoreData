ad7ee619966acee7f3fbe396dbad6e5ae5c45bd2
perry <perry091061@gmail.com>
2018-01-29 19:47:47 +0000

    Added Managed Object Model Document

--- Notes/Notes/Application Delegate/AppDelegate.swift
+++ Notes/Notes/Application Delegate/AppDelegate.swift
@@ -15,7 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let manager = CoreDataManager(modelName: "Notes")
+        
+        print(">> \(manager.managedObjectContext)")
+        
+        
         return true
     }
 
